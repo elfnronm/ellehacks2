80182c17010cc29db90d4dc0e4a494ee50768a1f
elif onem <elif_onem@msn.com>
2024-02-17 16:09:30 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("linktoabout").addEventListener("click", function() {
+    console.log("About link clicked"); // Check if the event listener is triggered
+
+    window.location.href = "about.html"; // Yeni sayfaya yönlendirme
+});
